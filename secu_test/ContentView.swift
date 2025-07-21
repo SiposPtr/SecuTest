5a3418f6f73d2246edadeab2c7f848d3b6e98b18
Peter Sipos <sipos.peter@sysconet.hu>
2025-07-21 18:42:19 +0200

    Initial Commit

---
+++ secu_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  secu_test
+//
+//  Created by Péter Sipos on 2025. 07. 21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ secu_test/secu_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  secu_testApp.swift
+//  secu_test
+//
+//  Created by Péter Sipos on 2025. 07. 21..
+//
+
+import SwiftUI
+
+@main
+struct secu_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
